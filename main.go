9d8fa5475dc0c5a89a640482195040c17e959560
Sergey Chulanov <tty8747@gmail.com>
2021-11-27 22:31:46 +0300

    Get ISO 3166-1 alpha-3 country code

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	var d Data
+	resp, err := http.Get("https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2021-11-01/2021-11-02")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	// jDecoder(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &d)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(d.Countries)
+}
+
+type Data struct {
+	// curl "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2021-11-01/2021-11-12" | jq '.countries[]'
+	Countries []string `json:"countries"`
+}
+
+func jDecoder(r io.ReadCloser) {
+	io.Copy(os.Stdout, r)
 }
