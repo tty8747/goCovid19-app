adad7a1d6084f93b8b46dd68036508f9f8936d28
Sergey Chulanov <tty8747@gmail.com>
2021-11-28 11:55:49 +0300

    Added buildLink func

--- main.go
+++ main.go
@@ -3,38 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"time"
 )
 
 func main() {
-	var d Data
-	resp, err := http.Get("https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2021-11-01/2021-11-02")
+	var c Countries
+	resp, err := http.Get(buildLink())
 	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
-	// jDecoder(resp.Body)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	err = json.Unmarshal(body, &d)
+	err = json.Unmarshal(body, &c)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(d.Countries)
+	fmt.Println(c.Countries[2])
 }
 
-type Data struct {
+type Countries struct {
 	// curl "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2021-11-01/2021-11-12" | jq '.countries[]'
 	Countries []string `json:"countries"`
 }
 
-func jDecoder(r io.ReadCloser) {
-	io.Copy(os.Stdout, r)
+func buildLink() string {
+	var link string = "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range"
+	tTime := time.Now()
+	return fmt.Sprintf("%s/%s-01-01/%s", link, tTime.Format("2006"), tTime.Format("2006-01-02"))
 }
