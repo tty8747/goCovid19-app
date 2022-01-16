c813e3952e32bd23093a4e4ffc79c7e6a9f8c274
Sergey Chulanov <tty8747@gmail.com>
2022-01-16 21:40:52 +0300

    Fixed mistake

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (app *application) serverErr(w http.ResponseWriter, err error) {
-	trace := fmt.Sprintf("%s\n%s", err.Error, debug.Stack())
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
