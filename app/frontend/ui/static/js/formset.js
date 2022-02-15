cdff9fa67dedebea3e938f3ea0771f998c8c4dac
Sergey Chulanov <tty8747@gmail.com>
2022-02-15 20:47:29 +0300

    Fixed js code

--- app/frontend/ui/static/js/formset.js
+++ app/frontend/ui/static/js/formset.js
@@ -7,8 +7,10 @@ let radioDD = document.getElementsByName('radioDD')
 document.addEventListener('submit', () => {
 	sessionStorage.setItem('date_from', dateFrom.value)
 	sessionStorage.setItem('date_to', dateTo.value)
+//  if (countrySelect.value == "") {
+//	  countrySelect.value = "default"
+//  }
 	sessionStorage.setItem('country_select', countrySelect.value)
-	
 	radioDD.forEach(button => {
 		if (button.checked) {
 			sessionStorage.setItem('radio_dd', button.value)
@@ -18,12 +20,12 @@ document.addEventListener('submit', () => {
 
 dateFrom.value = sessionStorage.getItem('date_from')
 dateTo.value = sessionStorage.getItem('date_to')
-countrySelect.value = sessionStorage.getItem('country_select')
+if (sessionStorage.getItem('country_select') == null) {
+  countrySelect.value = 'default'
+} else {
+  countrySelect.value = sessionStorage.getItem('country_select')
+}
 
 let radioValue = sessionStorage.getItem('radio_dd')
 let radioButton = document.querySelector(`input[value = "${radioValue}"]`)
 radioButton.checked = true
-
-// console.log(select.options[0].label)
-// console.log(select.options)
-// console.log(radioButton)
